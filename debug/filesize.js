9dc94559661bde5b7d6de462cda2c7522c5281a0
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-05-08 15:22:42 -0400

    Getting the node.js export correct (i think)

--- debug/filesize.js
+++ debug/filesize.js
@@ -32,7 +32,7 @@
  * 
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @module filesize
- * @version 1.6.2
+ * @version 1.6.3
  * 
  * @param  {Mixed}   arg   String, Int or Float to transform
  * @param  {Number}  pos   [Optional] Position to round to, defaults to 2 if short is ommitted
@@ -86,7 +86,7 @@
 
 	switch (true) {
 		case typeof exports !== "undefined":
-			exports.filesize = filesize;
+			module.exports = filesize;
 			break;
 		case typeof define === "function":
 			define("filesize", function () { return filesize; });
