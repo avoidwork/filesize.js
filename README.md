39e65291d363a59ec4e8b347ccf39ebc9864dc39
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-08-15 06:52:08 -0400

    Implemented a grunt build process & unit tests

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # filesize.js
+
 filesize.js provides a simple way to get a human readable file size string from a number (float or integer) or string.  An optional second parameter is the decimal place to round to (default is 2), or _true_ which triggers Unix style output. The maximum supported size is a terabyte. When hard drives get bigger, I'll add support for petabytes.
 
 ## Examples
+
 ``` js
 filesize(1500);                   // "1.46KB"
 filesize("1500000000");           // "1.40GB"
@@ -11,11 +13,15 @@ filesize(1212312421412412, true); // "1102.6T" - shorthand output, similar to *n
 ```
 
 ## How can I load filesize.js?
+
 filesize.js supports AMD loaders (require.js, curl.js, etc.), node.js & npm (npm install filesize), or using a script tag.
 
 ## Information
+
 #### License
+
 filesize.js is licensed under BSD-3 http://www.opensource.org/licenses/BSD-3-Clause
 
 #### Copyright
+
 Copyright (c) 2012, Jason Mulligan <jason.mulligan@avoidwork.com>
---
+++ src/filesize.js
@@ -0,0 +1,59 @@
+(function (global) {
+	"use strict";
+
+	/**
+	 * filesize
+	 * 
+	 * @param  {Mixed}   arg   String, Int or Float to transform
+	 * @param  {Number}  pos   [Optional] Position to round to, defaults to 2 if short is ommitted
+	 * @param  {Boolean} short [Optional] Shorthand output, similar to "ls -lh", overrides pos to 1
+	 * @return {String}        Readable file size String
+	 */
+	var filesize = function (arg) {
+		var pos, short, num, sizes, size, result, regex, suffix, i, z;
+
+		if (typeof arguments[2] !== "undefined") {
+			pos   = arguments[1];
+			short = arguments[2];
+		}
+		else typeof arguments[1] === "boolean" ? short = arguments[1] : pos = arguments[1];
+
+		if (isNaN(arg) || (typeof pos !== "undefined" && isNaN(pos))) throw Error("Invalid arguments");
+
+		short  = (short === true);
+		pos    = short ? 1 : (typeof pos === "undefined" ? 2 : parseInt(pos));
+		num    = String(arg).indexOf(".") > -1 ? parseFloat(arg) : parseInt(arg);
+		sizes  = [["B", 0], ["KB", 1024], ["MB", 1048576], ["GB", 1073741824], ["TB", 1099511627776]];
+		i      = sizes.length;
+		result = "";
+		regex  = /\.(.*)/;
+
+		while (i--) {
+			size   = sizes[i][1];
+			suffix = sizes[i][0];
+			if (num >= size) {
+				result = (suffix === "B" ? num : (num / size)).toFixed(pos);
+				if (short) {
+					suffix = suffix.slice(0, 1);
+					z      = regex.exec(result);
+					if (z !== null && typeof z[1] !== "undefined" && z[1] === "0") result = parseInt(result);
+				}
+				result += suffix;
+				break;
+			}
+		}
+
+		return result;
+	};
+
+	switch (true) {
+		case typeof exports !== "undefined":
+			module.exports = filesize;
+			break;
+		case typeof define === "function":
+			define(function () { return filesize; });
+			break;
+		default:
+			global.filesize  = filesize;
+	}
+})(this);
