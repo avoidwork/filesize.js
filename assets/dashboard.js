1283f50e90e1e2f4fc7348c5b13da7c8cc928402
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-02-02 14:59:24 -0500

    Leveraging the CDNs and removing require.js

---
+++ assets/dashboard.js
@@ -0,0 +1,21 @@
+/**
+ * filesize.js dashboard
+ *
+ * @author Jason Mulligan <jason.mulligan@avoidwork.com>
+ * @version 1.2
+ */
+(function (global) {
+	"use strict";
+
+	$.on("render", function () {
+		// Setting viewport to maintain required size (until media queries are in place)
+		if ($.client.mobile || $.client.tablet) $("head").create("meta", {name: "viewport", content: "width=1200"});
+
+		// DOM decoration
+		$("year").text(new Date().getFullYear());
+		$(".amd").on("click", function (e) { location = "https://github.com/amdjs/amdjs-api/wiki/AMD"; });
+		$(".node").on("click", function (e) { location = "http://nodejs.org"; });
+		$(".license").on("click", function (e) { location = "http://www.opensource.org/licenses/BSD-3-Clause"; });
+		$("body").css("opacity", 1);
+	}, "gui");
+})(this);
