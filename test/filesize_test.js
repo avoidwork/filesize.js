7a3aa089fd3cfd17244146e70699083406550cdb
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-04-07 08:05:19 -0400

    Added a `third` optional boolean argument to disable `bit` sizes, fixes #25
    Added a line return

--- test/filesize_test.js
+++ test/filesize_test.js
@@ -12,24 +12,42 @@ exports["filesize"] = {
 		done();
 	},
 	valid: function (test) {
-		test.expect(16);
-		test.equal(filesize(this.Kb),         "3.91Kb",  "Should match");
-		test.equal(filesize(this.Kb,true),    "3.9k",    "Should match");
-		test.equal(filesize(this.num),        "1.00KB",  "Should match");
-		test.equal(filesize(this.str),        "1.00KB",  "Should match");
-		test.equal(filesize(this.num, 1),     "1.0KB",   "Should match");
-		test.equal(filesize(this.str, 1),     "1.0KB",   "Should match");
-		test.equal(filesize(this.num, true),  "1K",      "Should match");
-		test.equal(filesize(this.str, true),  "1K",      "Should match");
-		test.equal(filesize(this.neg),        "-1.00KB", "Should match");
-		test.equal(filesize(this.neg, true),  "-1K",     "Should match");
-		test.equal(filesize(this.byte),       "1.00B",   "Should match");
-		test.equal(filesize(this.byte, 1),    "1.0B",    "Should match");
-		test.equal(filesize(this.byte, true), "1B",      "Should match");
-		test.equal(filesize(this.zero),       "0.00B",   "Should match");
-		test.equal(filesize(this.zero, 1),    "0.0B",    "Should match");
-		test.equal(filesize(this.zero, true), "0B",      "Should match");
-		this.byte  = 1;
+		test.expect(28);
+
+		test.equal(filesize(this.Kb),                "3.91Kb",   "Should be '3.91Kb'");
+		test.equal(filesize(this.Kb, 1),             "3.9Kb",    "Should be '3.9Kb'");
+		test.equal(filesize(this.Kb, 1, false),      "500.0B",   "Should be '500.0B'");
+		test.equal(filesize(this.Kb, true),          "3.9k",     "Should be '3.9k'");
+		test.equal(filesize(this.Kb, true, false),   "500B",     "Should be '500B'");
+
+		test.equal(filesize(this.num),               "1.00KB",   "Should be '1.00KB'");
+		test.equal(filesize(this.num, 1),            "1.0KB",    "Should be '1.0KB'");
+		test.equal(filesize(this.num, 1, false),     "1.0KB",    "Should be '1.0KB'");
+		test.equal(filesize(this.num, true),         "1K",       "Should be '1K'");
+		test.equal(filesize(this.num, true, false),  "1K",       "Should be '1K'");
+
+		test.equal(filesize(this.str),               "1.00KB",   "Should be '1.00KB'");
+		test.equal(filesize(this.str, 1),            "1.0KB",    "Should be '1.0KB'");
+		test.equal(filesize(this.str, 1, false),     "1.0KB",    "Should be '1.0KB'");
+		test.equal(filesize(this.str, true),         "1K",       "Should be '1K'");
+		test.equal(filesize(this.str, true, false),  "1K",       "Should be '1K'");
+
+		test.equal(filesize(this.neg),               "-1.00KB",  "Should be '-1.00KB'");
+		test.equal(filesize(this.neg, 1),            "-1.0KB",   "Should be '-1.0KB'");
+		test.equal(filesize(this.neg, 1, false),     "-1.0KB",   "Should be '-1.0KB'");
+		test.equal(filesize(this.neg, true),         "-1K",      "Should be '-1KB'");
+		test.equal(filesize(this.neg, true, false),  "-1K",      "Should be '-1KB'");
+
+		test.equal(filesize(this.byte),              "1.00B",    "Should be '1.00B'");
+		test.equal(filesize(this.byte, 1),           "1.0B",     "Should be '1.0B'");
+		test.equal(filesize(this.byte, 1, false),    "1.0B",     "Should be '1.0B'");
+		test.equal(filesize(this.byte, true),        "1B",       "Should be '1B'");
+		test.equal(filesize(this.byte, true, false), "1B",       "Should be '1B'");
+
+		test.equal(filesize(this.zero),              "0.00B",    "Should be '0.00B'");
+		test.equal(filesize(this.zero, 1),           "0.0B",     "Should be '0.0B'");
+		test.equal(filesize(this.zero, true),        "0B",       "Should be '0B'");
+
 		test.done();
 	},
 	invalid: function (test) {
