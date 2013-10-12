1d5009854291ffb824bec86ec72e01f68c40faeb
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-10-12 09:24:03 -0400

    Updating website for 2.0.0
    Reverting a change

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -2,9 +2,9 @@
  * filesize.js dashboard
  *
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
- * @version 1.3.2
+ * @version 1.4.0
  */
-(function ($) {
+( function ( $ ) {
 	"use strict";
 
 	var result, input, demo;
@@ -17,55 +17,55 @@
 	function handler () {
 		var val = input.val();
 
-		if (val !== undefined && !val.toString().isEmpty()) {
+		if ( val !== undefined && !val.toString().isEmpty() ) {
 			try {
-				result.html(filesize(val, input.data("short"), input.data("bit")));
+				result.html( filesize( val, {unix: input.data( "unix" ), bits: input.data( "bits" ) } ) );
 			}
-			catch (e) {
-				result.html(e);
+			catch ( e ) {
+				result.html( e );
 			}
 		}
 		else {
-			result.html("&nbsp;");
+			result.html( "&nbsp;" );
 		}
 	};
 
 	// Setting event handler
-	$.on("ready", function () {
-		result = $("#result");
-		input  = $("#filesize");
-		demo   = $("#demo");
+	$.on( "ready", function () {
+		result = $( "#result" );
+		input  = $( "#filesize" );
+		demo   = $( "#demo" );
 
-		$("#year").text(new Date().getFullYear());
+		$( "#year" ).text( new Date().getFullYear() );
 
 		// Demo filters
-		demo.find(".clickable").on("click", function (e) {
-			var obj   = $.target(e),
-			    param = obj.data("param");
+		demo.find( ".clickable" ).on( "click" , function ( e ) {
+			var obj   = $.target( e ),
+			    param = obj.data( "param" );
 
-			if (obj.hasClass("icon-check-empty")) {
-				obj.removeClass("icon-check-empty").addClass("icon-check");
+			if ( obj.hasClass( "icon-check-empty" ) ) {
+				obj.removeClass( "icon-check-empty" ).addClass( "icon-check" );
 			}
 			else {
-				obj.removeClass("icon-check").addClass("icon-check-empty");
+				obj.removeClass( "icon-check" ).addClass( "icon-check-empty" );
 			}
 
-			input.data(param, !input.data(param));
+			input.data( param, !input.data( param ) );
 
 			handler();
 		}, "click");
 
 		// Capturing debounced input (125ms)
-		input.on("input", function (e) {
-			$.delay(function () {
+		input.on( "input", function ( e ) {
+			$.delay( function () {
 				handler();
-			}, 125, "keyUp");
-		}, "input");
+			}, 125, "keyUp" );
+		}, "input" );
 
 		// Halting form submission
-		$("form")[0].on("submit", function (e) {
-			$.stop(e);
+		$( "form" )[0].on( "submit", function ( e ) {
+			$.stop( e );
 			return false;
-		});
-	}, "gui");
-})(abaaso);
+		} );
+	}, "gui" );
+} )( abaaso );
