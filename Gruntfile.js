26d7a759e9184dda7292ed5c1c529321d62ceb6a
Rouven Weßling <rouven@contentful.com>
2020-12-05 23:28:33 +0100

    Upgrade babel to version 7

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
 		"babel": {
 			options: {
 				sourceMap: false,
-				presets: ["babel-preset-env"]
+				presets: ["@babel/preset-env"]
 			},
 			dist: {
 				files: {
