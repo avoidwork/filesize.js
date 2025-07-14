d372bab063001cdcf465f5c16d5a38ce2211ac46
Jason Mulligan <jason.mulligan@avoidwork.com>
2025-07-13 21:13:02 -0400

    Version bump, updating CHANGELOG.md

--- dist/filesize.js
+++ dist/filesize.js
@@ -3,7 +3,7 @@
  *
  * @copyright 2025 Jason Mulligan <jason.mulligan@avoidwork.com>
  * @license BSD-3-Clause
- * @version 11.0.0
+ * @version 11.0.1
  */
 // Error Messages
 const INVALID_NUMBER = "Invalid number";
